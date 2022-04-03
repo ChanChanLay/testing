9c894583f6860d867504256afc11a332903f20c3
chanlay <ayechan.m.cnsa@gmail.com>
2022-04-03 12:15:17 +0630

    add button and link

--- TestGitHubProject/TestGitHubProject/WF_test.aspx.designer.cs
+++ TestGitHubProject/TestGitHubProject/WF_test.aspx.designer.cs
@@ -20,5 +20,14 @@ namespace TestGitHubProject {
         /// 変更するには、フィールドの宣言をデザイナー ファイルから分離コード ファイルに移動します。
         /// </remarks>
         protected global::System.Web.UI.HtmlControls.HtmlForm form1;
+        
+        /// <summary>
+        /// BT_test コントロール。
+        /// </summary>
+        /// <remarks>
+        /// 自動生成されたフィールド。
+        /// 変更するには、フィールドの宣言をデザイナー ファイルから分離コード ファイルに移動します。
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button BT_test;
     }
 }
